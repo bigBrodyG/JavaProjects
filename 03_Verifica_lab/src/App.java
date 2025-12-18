a7e9278f32aadeaf1a280aae874efd584769c82a
Giordii <gfornari.casa@gmail.com>
2025-12-18 09:05:43 +0100

    ok

---
+++ 03_Verifica_lab/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
